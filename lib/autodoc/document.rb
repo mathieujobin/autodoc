d86303302af1fe90d8bca62492dd8eef1aa07037
Isamu Arimoto <isamu@to-kyo.to>
2015-11-18 08:57:40 +0900

    sinatra support

--- lib/autodoc/document.rb
+++ lib/autodoc/document.rb
@@ -173,8 +173,6 @@ module Autodoc
     def response_body_parsed_as_json
       JSON.pretty_generate(JSON.parse(response.body))
     rescue JSON::ParserError
-      "\n" + Nokogiri::XML(response_body_raw){ |config| config.strict }.to_xml
-    rescue Nokogiri::XML::SyntaxError
     end
 
     def controller
@@ -211,35 +209,6 @@ module Autodoc
       end
     end
 
-    def param2string params
-      param = []
-      params.each_pair{|key, val|
-        if val.is_a?(Array)
-          val.each{|val2|
-            param << "#{key}[]=#{val2}"
-          }
-        elsif val.is_a?(Hash)
-          val.each_pair{|key2, val2|
-            param << "#{key}[#{key2}]=#{val2}"
-          }
-        else
-          param << "#{key}=#{val}"
-        end
-      }
-      param.join('&')
-    end
-    def example_get_section 
-      if request.GET.present?
-        "\n### example GET\n#{param2string(request.GET)}\n"
-      end
-    end
-    
-    def example_post_section
-      if request.POST.present?
-        "\n### example POST\n#{param2string(request.POST)}\n"
-      end
-    end
-
     def parameters
       validators.map {|validator| Parameter.new(validator) }.join("\n")
     end
@@ -249,11 +218,7 @@ module Autodoc
     end
 
     def validators
-      if defined?(Sinatra)
-        WeakParameters.stats[method][request.env["PATH_INFO"]].try(:validators)
-      else
-        WeakParameters.stats[controller][action].try(:validators)
-      end
+      WeakParameters.stats[method][path].try(:validators)
     end
 
     class Parameter
@@ -295,7 +260,7 @@ module Autodoc
       end
 
       def assets
-        @assets ||= [required, only, except, comment].compact
+        @assets ||= [required, only, except].compact
       end
 
       def required
@@ -308,10 +273,6 @@ module Autodoc
 
       def except
         "except: `#{validator.options[:except].inspect}`" if validator.options[:except]
-      end
-      
-      def comment
-        "comment: `#{validator.options[:comment].inspect}`" if validator.options[:comment]
       end
     end
   end
